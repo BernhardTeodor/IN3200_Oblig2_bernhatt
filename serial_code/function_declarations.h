e232b55517ee87c8239d99edc2e1ceccea8621a4
Bernhard Teodor Thodesen <thodesen45@gmail.com>
2026-04-23 07:33:03 +0200

    Lagde serial_main, header fil og la til image struct

---
+++ serial_code/function_declarations.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_DECLARATIONS
+#define FUNCTION_DECLARATIONS
+
+typedef struct
+{
+    float** image_data; /* a 2D array of floats */
+    int m; /* # pixels in vertical-direction */
+    int n; /* # pixels in horizontal-direction */
+} 
+image;
+
+
+#endif
