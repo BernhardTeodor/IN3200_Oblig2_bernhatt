ed526d55cbeee322dae2eaec6864094ba974bf4c
Bernhard Teodor Thodesen <thodesen45@gmail.com>
2026-04-27 07:21:48 +0200

    Iso i header of funksjonskall

---
+++ serial_code/test_iso_diffusion.c
@@ -0,0 +1,18 @@
+#include "function_declarations.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+
+void test_iso_diffusion_denoising()
+{
+
+
+}
+
+int main()
+{
+
+    return(0);
+}
